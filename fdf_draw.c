1493c687501378cf77d32ccce64f00c58011df9d
jmykkane <jmykkane@student.hive.fi>
2023-01-02 13:40:27 +0200

    working in school pc now

--- fdf_draw.c
+++ fdf_draw.c
@@ -18,24 +18,27 @@ static void	get_next_points(t_fdf_data_set *s, char dir)
 	char	**x_values;
 	char	**y_values;
 
-	if (s->p.c < s->p.x_max - 1 && s->p.y < s->p.y_max - 1)
+	if (s->p.c < s->p.x_max - 1)
 	{
 		x_values = ft_split(s->d.arr[s->p.y], ' ');
-		y_values = ft_split(s->d.arr[s->p.y], ' ');
 		if (dir == 'r')
 		{
 			s->p_1.z = ft_atoi(x_values[s->p.c]);
 			s->p_2.z = ft_atoi(x_values[s->p.c + 1]);
+			free(x_values);
 		}
+	}
+	if (s->p.y < s->p.y_max - 1)
+	{
+		y_values = ft_split(s->d.arr[s->p.y], ' ');
 		if (dir == 'd')
 		{
 			s->p_1.z =  ft_atoi(y_values[s->p.c]);
 			free(y_values);
 			y_values = ft_split(s->d.arr[s->p.y + 1], ' ');
 			s->p_2.z = ft_atoi(y_values[s->p.c]);
+			free(y_values);
 		}
-		free(x_values);
-		free(y_values);
 	}
 }
 
@@ -47,7 +50,7 @@ void	draw(t_fdf_data_set s)
 {
 	s.p_1.x = (s.p.c * s.p.x_of) - (s.p.y * s.p.y_of) + s.p.x_t;
 	s.p_1.y = (((s.p.c * s.p.x_of) + (s.p.y * s.p.y_of)) / 2) + s.p.y_t;
-	if (s.p.c < s.p.x_max)
+	if (s.p.c < s.p.x_max - 1)
 	{
 		get_next_points(&s, 'r');
 		s.p_2.x = ((s.p.c + 1) * s.p.x_of) - (s.p.y * s.p.y_of) + s.p.x_t;
@@ -55,7 +58,7 @@ void	draw(t_fdf_data_set s)
 		s.p_2.y += s.p.y_t;
 		draw_line(s);
 	}
-	if (s.p.y < s.p.y_max)
+	if (s.p.y < s.p.y_max - 1)
 	{
 		get_next_points(&s, 'd');
 		s.p_2.x = (s.p.c * s.p.x_of) - ((s.p.y + 1) * s.p.y_of) + s.p.x_t;
