1cf3195a9caeae8f3fbd89749168d14f2e077dc7
jmykkane <jmykkane@student.hive.fi>
2023-01-02 13:28:14 +0200

    weird

--- fdf_draw.c
+++ fdf_draw.c
@@ -45,10 +45,9 @@ static void	get_next_points(t_fdf_data_set *s, char dir)
 // bresenham's algorithm
 void	draw(t_fdf_data_set s)
 {
-	ft_printf("Printing %d,%d \n", s.p.c, s.p.y);
 	s.p_1.x = (s.p.c * s.p.x_of) - (s.p.y * s.p.y_of) + s.p.x_t;
 	s.p_1.y = (((s.p.c * s.p.x_of) + (s.p.y * s.p.y_of)) / 2) + s.p.y_t;
-	if (s.p.c <= s.p.x_max)
+	if (s.p.c < s.p.x_max)
 	{
 		get_next_points(&s, 'r');
 		s.p_2.x = ((s.p.c + 1) * s.p.x_of) - (s.p.y * s.p.y_of) + s.p.x_t;
@@ -56,7 +55,7 @@ void	draw(t_fdf_data_set s)
 		s.p_2.y += s.p.y_t;
 		draw_line(s);
 	}
-	if (s.p.y <= s.p.y_max)
+	if (s.p.y < s.p.y_max)
 	{
 		get_next_points(&s, 'd');
 		s.p_2.x = (s.p.c * s.p.x_of) - ((s.p.y + 1) * s.p.y_of) + s.p.x_t;
@@ -70,12 +69,10 @@ void	draw(t_fdf_data_set s)
 void	graphic_engine(t_fdf_data_set s)
 {	
 	init_graphics(&s);
-	while (++s.p.y <= s.p.y_max)
+	while (++s.p.y < s.p.y_max)
 	{
-		ft_printf("y: %d max: %d\n", s.p.y, s.p.y_max);
-		while (s.p.c <= s.p.x_max)
+		while (s.p.c < s.p.x_max)
 		{
-			ft_printf("x: %d max: %d\n", s.p.c, s.p.x_max);
 			if (s.d.arr[s.p.y][++s.p.x] != ' ')
 			{
 				draw(s);
